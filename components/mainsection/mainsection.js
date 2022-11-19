0506d307661641f31d4743e4dd6e4ec6c9d49942
AtefeFazeli <AtefeFazeli.com>
2022-11-19 17:24:53 +0330

    push changes

---
+++ components/mainsection/mainsection.js
@@ -0,0 +1,8 @@
+class mainsection extends HTMLElement{
+    constructor(){
+        super()
+        this.innerHTML="hi user"
+    }
+
+}
+export {mainsection}
---
+++ js/app.js
@@ -0,0 +1,2 @@
+import { mainsection } from "../components/mainsection/mainsection.js";
+window.customElements.define('main-section',mainsection);
